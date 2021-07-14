b0661c4ee1786fc6fdd757249ad6e6d5a0e3ee4d
James <james.gordon1@verizon.com>
2021-07-14 16:23:08 -0500

    Updated files

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
-
+const 
 const app = express();
 const port = 3000;
+const{Restaraunts} = require('/myRestaraunt')
 
 app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
+})
+
+//flip a coin
+app.get('/flipcoin', function (req, res) {
+    const coin = (Math.floor(Math.random() * 2)==0);
+    if(coin) res.send('Heads');
+    else res.send('Tails');
+})
+
+app.get('/myRestaraunt', async (req, res) => {
+    const allRestaraunts = await Restaraunt.findAll();
+    res.json(allRes)
 })
